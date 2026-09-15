9e7a94f1f3f2d9ea64aeccc36cbea0fc880d81d1
rushcheyo <rushcheyo@users.noreply.github.com>
2026-09-15 16:49:25 +0000

    deploy: 52cbad63e6782eacc0b70f30630bfd258a0babdc

--- blog/sw.js
+++ blog/sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "xiaoyu-me-20251226112533";
+var CACHE_NAME = "xiaoyu-me-20260915164923";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/blog/css/pixyll.css?202512261125",
+        "/blog/css/pixyll.css?202609151649",
         "/blog/"
       ]);
     })
